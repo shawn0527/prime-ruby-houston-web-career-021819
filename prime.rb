6557f176814b55aad1a2564ad7f4ee5ed314302d
Shawn Ren <doggy-ren@hotmail.com>
2019-02-13 04:42:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime? (number) # Add  code here!
+counter = 2
+  if number <= 1
+    return false
+  end
+  while counter < number
+    if number % counter == 0
+      return false
+    end
+    counter += 1
+    break if counter == number
+  end
+    return true
+end
