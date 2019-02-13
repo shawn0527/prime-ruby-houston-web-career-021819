34c7cad254f4f58902d1224f33352fe92049736c
Shawn Ren <doggy-ren@hotmail.com>
2019-02-13 04:10:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 def prime? (number) # Add  code here!
-  require 'prime'
-  number.prime?
+  array=[]
+  new_array = array.push(1..number)
+  counter = 0
+  loop do
+    if number % new_array[counter] == 0
+      test_array = Array.new.push(new_array[counter])
+      counter += 1
+    else
+  end
+
+  if test_array == [1, number]
+    return 'true'
+  else
+    return 'false'
 end
